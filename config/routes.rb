e5ebaef97f11e0eb56b4b8f40d8cf28c89d5dbcc
lucasspain13 <lucas-spain@uiowa.edu>
2021-11-10 09:35:38 -0600

    Added bootstrap gem and switched homepage to home directory

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   resources :forms
   resources :events
   resources :inventories
@@ -10,7 +12,7 @@ Rails.application.routes.draw do
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  root "users#index"
+  root "home#index"
   resources :users
   resources :students
   resources :parents
