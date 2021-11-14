42fba8bd3930e3c708d72645877eb3db3ca34c98
Ella Norton <ella-norton@uiowa.edu>
2021-11-14 16:10:59 -0600

    email uniqueness verification implemented

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,7 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true
-  # , format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password, presence: true
   validates :password_confirmation, presence: true
   validates :password, confirmation: { case_sensitive: true }
