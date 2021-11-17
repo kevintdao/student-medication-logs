1561e3d9d8cbd2cb0c829835cba4d4c80708881c
spaghettinoodles <49127161+spaghettinoodles@users.noreply.github.com>
2021-11-16 18:46:01 -0600

    Added seeder for events DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,3 +21,18 @@ csv.each do |row|
 end
 
 puts "There are now #{Medication.count} rows in the transactions table"
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'events.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+
+csv.each do |row|
+  m = Event.new
+  m.student_id = row['StudentID']
+  m.nurse_id = row['NurseID']
+  m.med_id = row['MedicationID']
+  m.start_time = row['StartTime']
+  m.end_time = row['EndTime']
+  m.notes = row['Notes']
+  m.completed = row['Completed']
+  m.save
+end
