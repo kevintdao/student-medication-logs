f673b3fc75bc0aff65f03f95d8db8a7e4a5e5eee
lucasspain13 <lucas-spain@uiowa.edu>
2021-11-17 16:56:39 -0600

    Finished all rspec tests for homecontroller

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -40,9 +40,18 @@ RSpec.describe "Homes", type: :request do
     end
   end
   describe "POST /send_contact_message" do
-    it "returns http redirect" do
+    it "returns http redirect and flashes a notice" do
       post "/home/send_contact_message", message: {:name => "Test Name", :email => "mail@test.com", :subject => "TEST", :message => "This is a message?"}
       expect(response).to have_http_status(302)
+      expect(flash[:notice]).to be_present
+    end
+    it "flashes a warning for blank input" do
+      post "/home/send_contact_message", message: {:name => "", :email => "mail@test.com", :subject => "TEST", :message => "This is a message?"}
+      expect(flash[:warning]).to be_present
+    end
+    it "flashes a warning for invalid email" do
+      post "/home/send_contact_message", message: {:name => "Test", :email => "mailtestcom", :subject => "TEST", :message => "This is a message?"}
+      expect(flash[:warning]).to be_present
     end
   end
 end
