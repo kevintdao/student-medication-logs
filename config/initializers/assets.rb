d27a599f266ef1249ee8c5ccb86724ae173cd276
lucasspain13 <lucas-spain@uiowa.edu>
2021-11-17 14:00:27 -0600

    Completed notes entry, Added styles, fixed navigation bugs, switched from displaying med id to med name

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,6 +8,7 @@ Rails.application.config.assets.precompile += %w( contact.scss )
 Rails.application.config.assets.precompile += %w( medications.scss )
 Rails.application.config.assets.precompile += %w( users.scss )
 Rails.application.config.assets.precompile += %w( nurses.scss )
+Rails.application.config.assets.precompile += %w( events.scss )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
