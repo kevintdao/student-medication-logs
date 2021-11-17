4e5289f1b766fc9d7c685a0096feded0d89fbdb5
lucasspain13 <lucas-spain@uiowa.edu>
2021-11-17 16:51:55 -0600

    Finished rspec happy path controller methods

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -6,6 +6,43 @@ RSpec.describe "Homes", type: :request do
       get "/home/index"
       expect(response).to have_http_status(:success)
     end
+    it 'should clear the search_term from the session' do
+      get "/home/index"
+      expect(session[:search_term]).to be_nil
+    end
   end
 
+  describe "GET /about" do
+    it "returns http success" do
+      get "/home/about"
+      expect(response).to have_http_status(:success)
+    end
+    it 'should clear the search_term from the session' do
+      get "/home/about"
+      expect(session[:search_term]).to be_nil
+    end
+  end
+
+  describe "GET /contact" do
+    it "returns http success" do
+      get "/home/contact"
+      expect(response).to have_http_status(:success)
+    end
+    it 'should clear the search_term from the session' do
+      get "/home/contact"
+      expect(session[:search_term]).to be_nil
+    end
+  end
+  describe "GET /medications" do
+    it "returns http success" do
+      get "/home/medications"
+      expect(response).to have_http_status(:success)
+    end
+  end
+  describe "POST /send_contact_message" do
+    it "returns http redirect" do
+      post "/home/send_contact_message", message: {:name => "Test Name", :email => "mail@test.com", :subject => "TEST", :message => "This is a message?"}
+      expect(response).to have_http_status(302)
+    end
+  end
 end
