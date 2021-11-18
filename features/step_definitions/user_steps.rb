962e4a809ebe05b13a64d907519f7514a4919d94
Kevin Dao <kevin-dao@uiowa.edu>
2021-11-17 19:41:25 -0600

    Add cucumber test for search user feature

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -8,16 +8,16 @@ Given /^I am on the Users page$/ do
   visit 'users'
 end
 
-When /^I search by (.*) for the term (.*)$/ do |type, term|
+When /^I search by "(.*)" for the term "(.*)"$/ do |type, term|
   select(type, from: 'search_type')
-  fill_in('search_term', with: 'term')
+  fill_in('search_term', with: term)
   click_button('Search')
 end
 
-Then /^I should see user with the name of (.*)$/ do |name|
-  expect(page).to have_content(name)
+Then /^I should see user with the name of "(.*)"$/ do |name|
+  expect(page).to have_xpath("//td[text()='#{name}']")
 end
 
-Then /^I should not see user with the name of (.*)$/ do |name|
-  expect(page).not_to have_content(name)
+Then /^I should not see user with the name of "(.*)"$/ do |name|
+  expect(page).not_to have_xpath("//td[text()='#{name}']")
 end
