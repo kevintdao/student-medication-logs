dab93f1116d36da56f0ffe31aa1c9610b6fe6db6
Ella Norton <ella-norton@uiowa.edu>
2021-11-18 15:52:55 -0600

    CreateUser cucumber features and step definitions complete and passing

---
+++ features/step_definitions/create_user_steps.rb
@@ -0,0 +1,25 @@
+
+Given /^I am on the create users page$/ do
+  visit 'users/new'
+end
+
+When /^I click Invite User$/ do
+  click_button('Invite User')
+end
+
+When /^I click Set Password$/ do
+  click_button('Set Password')
+end
+
+When /^I select "(.*?)" from "(.*?)"$/ do |value, field|
+  select(value, :from => field)
+end
+
+Then /^The database should contain a user with the email "(.*?)"$/ do |email|
+  user = User.where(:email => email)
+  expect(user.length).not_to eq(0)
+end
+
+Then /^I should see "(.*?)"$/ do |text|
+  expect(page.has_content?(text)).to be_truthy
+end
