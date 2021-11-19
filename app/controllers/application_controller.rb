46c95009481918d6c1720bbe144a94357efd0ccc
afilerio <alfredofilerio@gmail.com>
2021-11-18 18:25:21 -0600

    added set_current_user function to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_filter :set_current_user
   protect_from_forgery with: :exception
   def index; end
+
+  def set_current_user
+    @current_user ||= User.where(:id=> session[:user_id])
+    redirect_to users_login_path and return unless @current_user
+  end
 end
