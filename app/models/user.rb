42b6306f706ba948df08c5297f44abd8077b7c9b
Ella Norton <ella-norton@uiowa.edu>
2021-11-18 19:56:34 -0600

    validate that a role for the User is chosen

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :role, presence: true
   validates :password, presence: true
   validates :password_confirmation, presence: true
   validates :password, confirmation: { case_sensitive: true }
