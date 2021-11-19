082403aafddade07362bdd4daa5a091101346c6d
Kevin Dao <kevin-dao@uiowa.edu>
2021-11-18 19:44:38 -0600

    Add login routes and remove duplicate routes
    Co-authored-by: Alfredo Filerio <alfredofilerio@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-
-  get 'users/login'
-
   get 'users/register'
-
-  get 'user/register'
 
   get 'home/contact'
 
@@ -22,16 +17,6 @@ Rails.application.routes.draw do
 
   post 'users/register_district_admin'
 
-  resources :districts
-  resources :forms
-  resources :events
-  resources :inventories
-  resources :medications
-  resources :admins
-  resources :nurses
-  resources :students
-  resources :parents
-  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   root "home#index"
@@ -46,9 +31,9 @@ Rails.application.routes.draw do
   resources :inventories
   resources :forms
 
-  resources :sessions, only: [:new, :create, :destroy]
 
-  match '/login', to: 'sessions#create', via: :get
+  match '/login', to: 'sessions#new', via: :get
+  match '/login_create', to: 'sessions#create', via: :post
   match '/logout', to: 'sessions#destroy', via: :delete
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
