3a9afdc07d8b39317027ace5cd7d0ce2532fafe3
Kevin Dao <kevin-dao@uiowa.edu>
2021-11-18 20:55:33 -0600

    Set current_user based on session token
    Co-authored-by: Alfredo Filerio <alfredofilerio@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   before_filter :set_current_user
   protect_from_forgery with: :exception
-  def index; end
 
   def set_current_user
-    @current_user ||= User.where(:id=> session[:user_id])
-    redirect_to users_login_path and return unless @current_user
+    @current_user ||= User.find_by_session_token(session[:session_token])
   end
 end
