e97e60ef409c3f726155ec6d68da1aff98203180
Kevin Dao <kevin-dao@uiowa.edu>
2021-11-19 17:56:39 -0600

    Fix rspec tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,34 +4,34 @@ require 'rails_helper'
 describe User do
   fixtures :users
   it 'should return all users when the search term is empty' do
-    users = User.search_users('Name', '')
+    users = User.search_users('Name', '', '1')
     expect(users[0].first_name).to eq('Admin')
     expect(users[0].last_name).to eq('Apple')
     expect(users.count).to eq(4)
   end
   context 'search by name' do
     it 'should return users with the searched first name' do
-      users = User.search_users('Name', 'Admin')
-      expect(users.count).to eq(2)
+      users = User.search_users('Name', 'Admin', '1')
+      expect(users.count).to eq(1)
     end
     it 'should return users with the searched first and last name' do
-      users = User.search_users('Name', 'Admin Apple')
+      users = User.search_users('Name', 'Admin Apple', '1')
       expect(users[0].first_name).to eq('Admin')
       expect(users[0].last_name).to eq('Apple')
       expect(users.count).to eq(1)
     end
     it 'should return nil when no users with searched name' do
-      users = User.search_users('Name', 'Admin Bob')
+      users = User.search_users('Name', 'Admin Bob', '1')
       expect(users.count).to eq(0)
     end
   end
   context 'search by role' do
     it 'should return users with the role name' do
-      users = User.search_users('Role', 'Nurse')
-      expect(users.count).to eq(2)
+      users = User.search_users('Role', 'Nurse', '1')
+      expect(users.count).to eq(1)
     end
     it 'should return nil when no users with the role name' do
-      users = User.search_users('Role', 'Coordinator')
+      users = User.search_users('Role', 'Coordinator', '1')
       expect(users.count).to eq(0)
     end
   end
