7ad6806538d6b2bc895c08bc45dbcc6782b7b106
Ella Norton <ella-norton@uiowa.edu>
2021-11-23 16:07:27 -0600

    add rspec tests to User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
+require 'spec_helper'
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+  describe 'send password' do
+    before :all do
+      @user = User.where(:email => 'studenta@gmail.com')[0]
+      @user.password = '123456'
+      @user.password_confirmation = '123456'
+      @user.save!
+    end
+    it 'should call generate token function' do
+      expect(@user).to receive(:generate_token)
+      @user.send_password_set
+    end
+    it 'should save new password_set_token and password_set_sent_at to the database' do
+      old_password_set_token = User.where(:email => 'studenta@gmail.com')[0].password_set_token
+      old_password_set_sent_at = User.where(:email => 'studenta@gmail.com')[0].password_set_sent_at
+      @user.send_password_set
+      new_password_set_token = User.where(:email => 'studenta@gmail.com')[0].password_set_token
+      new_password_set_sent_at = User.where(:email => 'studenta@gmail.com')[0].password_set_sent_at
+      expect(new_password_set_token).not_to eq(old_password_set_token)
+      expect(new_password_set_sent_at).not_to eq(old_password_set_sent_at)
+    end
+  end
 end
+
