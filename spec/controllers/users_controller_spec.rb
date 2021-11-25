5c827eacea2b7ccf4146ad0888e849a20b0b40e4
Kevin Dao <kevin-dao@uiowa.edu>
2021-11-24 23:30:36 -0600

    Fix rspec tests user_controllers

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -107,18 +107,24 @@ describe UsersController do
   end
 
   describe 'display users' do
-    fixtures :users
     context 'search users' do
+      it 'should redirect to login page if not logged in' do
+        get :index, {}
+        expect(response).to redirect_to(login_path)
+      end
       it 'should call the model method that search users' do
+        controller.instance_variable_set(:@current_user, User.where(email: 'admin1@gmail.com')[0])
         user = double('admin1')
         expect(User).to receive(:search_users).with('Name', 'admin', 1).and_return(user)
         get :index, { search: { type: 'Name', term: 'admin' } }
       end
       it 'should return all users when search term is empty' do
+        controller.instance_variable_set(:@current_user, User.where(email: 'admin1@gmail.com')[0])
         get :index, {}
         expect(response).to render_template('index')
       end
       it 'should flash error message if no users exists' do
+        controller.instance_variable_set(:@current_user, User.where(email: 'admin1@gmail.com')[0])
         allow(User).to receive(:search_users).with('Name', 'asdf', 1).and_return(nil)
         get :index, { search: { type: 'Name', term: 'asdf' } }
         expect(flash[:error]).to eq('No users found!')
