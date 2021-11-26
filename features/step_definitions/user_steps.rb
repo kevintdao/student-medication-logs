767b432cbdd80949027e5fb0f7ef7def4b17b9f4
Kevin Dao <kevin-dao@uiowa.edu>
2021-11-26 12:24:17 -0600

    Fix cucumber tests failing when user is not logged in and duplicate step definitions

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,11 +1,12 @@
-Given /the following users have been added to SML:/ do |users_table|
-  users_table.hashes.each do |user|
-    User.create!(user)
-  end
-end
-
 Given /^I am on the Users page$/ do
   visit 'users'
+end
+
+Given /^I am logged in as an admin$/ do
+  visit login_path
+  fill_in('loginEmail', with: 'admin1@gmail.com')
+  fill_in('loginPassword', with: '123456')
+  click_button('Login')
 end
 
 When /^I search by "(.*)" for the term "(.*)"$/ do |type, term|
@@ -20,4 +21,36 @@ end
 
 Then /^I should not see user with the name of "(.*)"$/ do |name|
   expect(page).not_to have_xpath("//td[text()='#{name}']")
+end
+
+Given /^I am on the nurse dashboard$/ do
+  visit nurses_path
+end
+
+Then /^I should see the nurse dashboard$/ do
+  expect(page.has_content?("Click the button below to see upcoming events.")).to be_truthy
+end
+
+Given /^I am on the parent dashboard$/ do
+  visit parents_path
+end
+
+Then /^I should see the parent dashboard$/ do
+  expect(page.has_content?("Click the button below to see upcoming events.")).to be_truthy
+end
+
+Given /^I am on the student dashboard$/ do
+  visit students_path
+end
+
+Then /^I should see the student dashboard$/ do
+  expect(page.has_content?("Click the button below to see upcoming events.")).to be_truthy
+end
+
+When /^I click the link called "(.*?)"$/ do |link|
+  click_link link
+end
+
+Then /^I should see a page with content "(.*?)"$/ do |content|
+  expect(page.has_content?(content)).to be_truthy
 end
