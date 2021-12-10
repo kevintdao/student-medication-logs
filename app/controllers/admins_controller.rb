b9ab31a6a6d05721b3ca6451ce9847a938cdbba5
Kevin Dao <kevin-dao@uiowa.edu>
2021-12-10 14:55:02 -0600

    Remove unused controller functions from student and admin controller

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -4,61 +4,7 @@ class AdminsController < ApplicationController
   # GET /admins
   # GET /admins.json
   def index
-    @admins = Admin.all
-  end
-
-  # GET /admins/1
-  # GET /admins/1.json
-  def show
-  end
-
-  # GET /admins/new
-  def new
-    @admin = Admin.new
-  end
-
-  # GET /admins/1/edit
-  def edit
-  end
-
-  # POST /admins
-  # POST /admins.json
-  def create
-    @admin = Admin.new(admin_params)
-
-    respond_to do |format|
-      if @admin.save
-        format.html { redirect_to @admin, notice: 'Admin was successfully created.' }
-        format.json { render :show, status: :created, location: @admin }
-      else
-        format.html { render :new }
-        format.json { render json: @admin.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /admins/1
-  # PATCH/PUT /admins/1.json
-  def update
-    respond_to do |format|
-      if @admin.update(admin_params)
-        format.html { redirect_to @admin, notice: 'Admin was successfully updated.' }
-        format.json { render :show, status: :ok, location: @admin }
-      else
-        format.html { render :edit }
-        format.json { render json: @admin.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /admins/1
-  # DELETE /admins/1.json
-  def destroy
-    @admin.destroy
-    respond_to do |format|
-      format.html { redirect_to admins_url, notice: 'Admin was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    @admin = @current_user
   end
 
   private
