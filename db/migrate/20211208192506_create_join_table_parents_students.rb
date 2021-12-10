82aa02b1e1c9d0d7da17ac6fb0b6df372a0c54f9
Kevin Dao <66343363+kevintdao@users.noreply.github.com>
2021-12-10 11:43:09 -0600

    Delete 20211208192506_create_join_table_parents_students.rb

--- db/migrate/20211208192506_create_join_table_parents_students.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableParentsStudents < ActiveRecord::Migration
-  def change
-    create_join_table :parents, :students do |t|
-      # t.index [:parent_id, :student_id]
-      # t.index [:student_id, :parent_id]
-    end
-  end
-end
