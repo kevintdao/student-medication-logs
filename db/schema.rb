69d5d1e3431a7b4a1fb6c7425b0ebd359486464a
afilerio <alfredofilerio@gmail.com>
2021-12-09 23:05:24 -0600

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20211205231131) do
+ActiveRecord::Schema.define(version: 20211206212649) do
 
   create_table "admins", force: :cascade do |t|
     t.datetime "created_at", null: false
