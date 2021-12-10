c18dddab27efee7c19a268c5c03ab420a1d2f129
afilerio <alfredofilerio@gmail.com>
2021-12-10 16:31:35 -0600

    removed already associated step

--- features/step_definitions/associate_steps.rb
+++ features/step_definitions/associate_steps.rb
@@ -4,15 +4,6 @@ When /^I enter "(.*)" into "(.*)" fields$/ do |name, field|
   fill_in("#{field}_last", with: name_split[1])
 end
 
-Given /^"(.*)" and "(.*)" are already associated$/ do |name1, name2|
-  parent_id = User.where(first_name: name2.split(' ')[0], last_name: name2.split(' ')[1])[0].role_id
-  student_id = User.where(first_name: name1.split(' ')[0], last_name: name1.split(' ')[1])[0].role_id
-  select(student_id, 'student')
-  select(parent_id, 'parent')
-  
-  click_button('Submit')
-end
-
 Given(/^I am on the associate page$/) do
   visit nurses_associate_page_path
 end
