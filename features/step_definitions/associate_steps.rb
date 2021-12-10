a987d74e54ca3298c92f102a1eecf5e8832e33bf
afilerio <alfredofilerio@gmail.com>
2021-12-10 16:16:57 -0600

    fixed When I select {} in {} menu step

--- features/step_definitions/associate_steps.rb
+++ features/step_definitions/associate_steps.rb
@@ -4,7 +4,7 @@ When /^I enter "(.*)" into "(.*)" fields$/ do |name, field|
   fill_in("#{field}_last", with: name_split[1])
 end
 
-Given /^"(.*)" and "(.*)" are already associated $/ do |name1, name2|
+Given /^"(.*)" and "(.*)" are already associated$/ do |name1, name2|
   parent_id = User.where(first_name: name2.split(' ')[0], last_name: name2.split(' ')[1])[0].role_id
   student_id = User.where(first_name: name1.split(' ')[0], last_name: name1.split(' ')[1])[0].role_id
   select(student_id, 'student')
@@ -18,6 +18,6 @@ Given(/^I am on the associate page$/) do
 end
 
 When /^I select "(.*)" in "(.*)" menu$/ do |name, menu|
-  role_id = User.where(first_name: name.split(' ')[0], last_name: name.split(' ')[1])[0].role_id
-  select(role_id, menu)
+  role_id = User.where(first_name: name.split(' ')[0], last_name: name.split(' ')[1])[0].role
+  select(role_id, :from => menu)
 end
