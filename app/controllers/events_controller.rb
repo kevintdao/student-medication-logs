323c1ba85752b5282d67254f5c7fe0f5697001ed
spaghettinoodles <49127161+spaghettinoodles@users.noreply.github.com>
2021-12-10 20:19:46 -0600

    added parent and student verification to events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,8 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
-  before_action :is_nurse, only: [:index, :past_events, :search_events, :search_past_events, :set_page_count, :set_past_page_count, :complete, :incomplete, :change_notes]
+  before_action :is_nurse, only: [:index, :past_events, :complete, :incomplete, :change_notes]
+  before_action :is_parent, only: [:parent_past_events]
+  before_action :is_student, only: [:student_past_events]
   before_action :belongs_to_district, only: [:show]
   # GET /events
   # GET /events.json
@@ -39,6 +41,52 @@ class EventsController < ApplicationController
         @events = Event.where(complete: true).where("lower(student_id) LIKE ? OR lower(med_id) LIKE ? OR lower(notes) LIKE ?", @selection.downcase, @selection.downcase, @selection.downcase).reorder("time ASC").page(params[:page]).per_page(@pages)
       end
     end
+  end
+
+  # GET /events/parent_past_events
+  def parent_past_events
+    @pages = session[:page_count]
+    @selection = session[:search_term]
+
+    @students = User.where(role: "Student", role_id: Parent.where(id: @current_user.role_id).first.student_ids).all
+    unless @students.nil?
+      @ids = @students.map{|s| s.id}
+      if @pages.nil?
+        if @selection.nil? or @selection.blank?
+          @events = Event.where(districtID: @current_user.district_id, studentID: @ids).page(params[:page]).per_page(50)
+        else
+          @events = Event.where(districtID: @current_user.district_id, studentID: @ids).where('body LIKE ?', @selection).all.page(params[:page]).per_page(50)
+        end
+      else
+        if @selection.nil? or @selection.blank?
+          @events = Event.where(districtID: @current_user.district_id, studentID: @ids).page(params[:page]).per_page(@pages)
+        else
+          @events = Event.where(districtID: @current_user.district_id, studentID: @ids).where('body LIKE ?', @selection).all.page(params[:page]).per_page(@pages)
+        end
+      end
+    else
+      flash[:warning] = "You have no students. Please contact the district admin."
+    end
+    render 'events/past_events'
+  end
+
+  def student_past_events
+    @pages = session[:page_count]
+    @selection = session[:search_term]
+    if @pages.nil?
+      if @selection.nil? or @selection.blank?
+        @events = Event.where(complete: true).reorder("time ASC").page(params[:page]).per_page(50)
+      else
+        @events = Event.where(complete: true).where("lower(student_id) LIKE ? OR lower(med_id) LIKE ? OR lower(notes) LIKE ?", @selection.downcase, @selection.downcase, @selection.downcase).reorder("time ASC").page(params[:page]).per_page(50)
+      end
+    else
+      if @selection.nil? or @selection.blank?
+        @events = Event.where(complete: true).reorder("time ASC").page(params[:page]).per_page(@pages)
+      else
+        @events = Event.where(complete: true).where("lower(student_id) LIKE ? OR lower(med_id) LIKE ? OR lower(notes) LIKE ?", @selection.downcase, @selection.downcase, @selection.downcase).reorder("time ASC").page(params[:page]).per_page(@pages)
+      end
+    end
+    render 'events/past_events'
   end
 
   def set_page_count
@@ -198,6 +246,36 @@ class EventsController < ApplicationController
         unless @current_user.role == 'Nurse'
           # The user is not a nurse
           flash[:warning] = "You must be a registered nurse to access this page."
+          redirect_to home_index_path
+        end
+      end
+    end
+
+    def is_parent
+      if @current_user.nil?
+        # There is no logged in user
+        flash[:warning] = "You must be logged in as a parent to access this page."
+        redirect_to home_index_path
+      else
+        # The user is logged in
+        unless @current_user.role == "Parent"
+          # The user is not a parent
+          flash[:warning] = "You must be a parent to access this page."
+          redirect_to home_index_path
+        end
+      end
+    end
+
+    def is_student
+      if @current_user.nil?
+        # There is no logged in user
+        flash[:warning] = "You must be logged in as a student to access this page."
+        redirect_to home_index_path
+      else
+        # The user is logged in
+        unless @current_user.role == "Student"
+          # The user is not a parent
+          flash[:warning] = "You must be a student to access this page."
           redirect_to home_index_path
         end
       end
